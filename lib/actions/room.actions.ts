5f2fe70fdbb115ae1e89c74b748ccf20f1559622
Rahman <museabdirahman@gmail.com>
2024-10-04 20:27:16 +0100

    intialised project

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { RoomAccesses } from "@liveblocks/node";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+// server action to create new document + room , to be called from front end when user clicks btn
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    // creating room :copied from https://liveblocks.io/docs/authentication/id-token/nextjs
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: [],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while creating room ${error}`);
+  }
+};
