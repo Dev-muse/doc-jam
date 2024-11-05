72fdba21ba2dfc5a9d3e263feeeb79ce597f191e
Rahman <museabdirahman@gmail.com>
2024-11-05 21:51:34 +0000

    user metion functionality added , at this point all of the live collaborative features are available . Just need to add feature to invite collaborators to take advantage of features

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.actions';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
+import { useUser } from '@clerk/nextjs';
 import {
   ClientSideSuspense,
   LiveblocksProvider,
@@ -10,12 +11,26 @@ import {
 import { ReactNode } from 'react';
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  // fetch clerk user
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint={'/api/liveblocks-auth'}
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      // find which users in which rooms for mentions feature
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        // server action to get users that belong to doc/room
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from '@clerk/nextjs/server';
 import { parseStringify } from '../utils';
+import { liveblocks } from '../liveblocks';
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
@@ -23,5 +24,39 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     return parseStringify(sortedUsers);
   } catch (error) {
     console.log('error fetching users ', error);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    // if we're mentioning someone convert txt to lowercase
+    if (text.length) {
+      const lowercaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowercaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+
+    // if no text being searched just return any user
+    return parseStringify(users);
+  } catch (error) {
+    console.log('error getting user information', error);
   }
 };
