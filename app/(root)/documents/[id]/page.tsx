3570e938b2a2d0ce78f4f342bc42ea64b62a9170
Rahman <museabdirahman@gmail.com>
2025-11-07 01:26:52 +0000

    fix clerk related build error due to null user email when accessing doc

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,39 +1,46 @@
-import CollaborativeRoom from '@/components/CollaborativeRoom';
-import { getDocument } from '@/lib/actions/room.actions';
-import { getClerkUsers } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
   // 1- get id of room from params , dynamic route
 
   const clerkUser = await currentUser();
-  if (!clerkUser) redirect('/sign-in');
+
+  const clerkUserEmail = clerkUser?.emailAddresses?.[0]?.emailAddress;
+  if (!clerkUserEmail) redirect("/sign-in");
 
   // get access to room/document
   const room = await getDocument({
     roomId: id,
-    userId: clerkUser.emailAddresses[0].emailAddress,
+    userId: clerkUserEmail,
   });
 
-  if (!room) redirect('/');
+  if (!room) redirect("/");
   //TODO: assess the permisssion lvl of user to access the document/room
 
+  // fetch all users in the room
   const userIds = Object.keys(room.usersAccesses);
   const users = await getClerkUsers({ userIds });
 
-  const userData = users.map((user: User) => ({
-    ...user,
-    userType: room.usersAccesses[user.email]?.includes('room:write')
-      ? 'editor'
-      : 'viewer',
-  }));
+  const userData = users.map((user: User) => {
+     const email = user?.email;
+    return {
+      ...user,
+      userType:
+        email && room.usersAccesses[email]?.includes("room:write")
+          ? "editor"
+          : "viewer",
+    };
+  });
 
-  const currentUserType = room.usersAccesses[
-    clerkUser.emailAddresses[0].emailAddress
-  ]?.includes('room:write')
-    ? 'editor'
-    : 'viewer';
+  const currentUserType = room.usersAccesses[clerkUserEmail]?.includes(
+    "room:write"
+  )
+    ? "editor"
+    : "viewer";
 
   return (
     <main className="flex w-full flex-col items-center">
